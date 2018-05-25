35c166b93e2425d9c0e087c27c678f826c80ab27
masaldi <33805900+masaldi@users.noreply.github.com>
2018-05-26 00:59:36 +0700

    Update bot-api-config.php

--- bot-api-config.php
+++ bot-api-config.php
@@ -4,7 +4,7 @@ if (!defined('HS')) {
     die('Tidak boleh diakses langsung.');
 }
 
-$token = '';
+$token = '436856656:AAG6NITsiHQkTAhaTgHoaKa9lQHMNA5Kjx8';
 $debug = true;
 
 $chatid = '213567634';
